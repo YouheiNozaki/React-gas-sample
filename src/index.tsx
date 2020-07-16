e12604717978c5847ab60f4b06024cb0d51b3fb2
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-07-16 22:15:15 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#app'));
